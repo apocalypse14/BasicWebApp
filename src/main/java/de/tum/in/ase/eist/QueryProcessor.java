c289ca9052aac6cf3f0e9136d5093928a727f27c
Kagan Durmus <kagandurmus2001@gmail.com>
2022-07-14 15:28:21 +0200

    pluswdf

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -15,7 +15,7 @@ public class QueryProcessor {
             return "kagan";
         } else if (query.contains("plus")) {
             query = query.replaceAll(" +", "plus");
-            return query;
+            return process(query);
 
         } else { // TODO extend the programm here
             return "";
